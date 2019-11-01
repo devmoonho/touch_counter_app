6c07ea971a843d814fd07103a01c7e02988c22c4
dev.moonho@gmail.com <lgcns2020!!>
2019-11-01 21:36:45 +0900

    배경이미지 적용 Scaffold 투명도 적용

--- lib/app_pages/home.dart
+++ lib/app_pages/home.dart
@@ -4,7 +4,20 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/TouchCounter_bg.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Opacity(
+        opacity: 0.0,
+        child: Scaffold(
+          body: Column(
 
+          ),
+        ),
+      ),
     );
   }
 }
