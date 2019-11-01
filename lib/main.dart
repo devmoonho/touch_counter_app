3800ed2e40b7977af9c30a6c67346cf444b0ed65
dev.moonho@gmail.com <lgcns2020!!>
2019-11-01 21:09:36 +0900

    Home widget 생성 MaterialApp widget 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'app_pages/home.dart';
+
 void main() => runApp(TouchCounterApp());
 
 class TouchCounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return MaterialApp(
+      title: 'Touch Counter App',
+      home: Home(),
     );
   }
 }
