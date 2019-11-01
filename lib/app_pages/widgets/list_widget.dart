b25eb3a9bfb98ee147d54450c9c5be41878400e0
dev.moonho@gmail.com <lgcns2020!!>
2019-11-02 06:54:17 +0900

    list 위젯 패딩추가 list 위젯 폰트 크게

--- lib/app_pages/widgets/list_widget.dart
+++ lib/app_pages/widgets/list_widget.dart
@@ -5,33 +5,47 @@ import 'package:touch_counter_app/models/touch_counter_model.dart';
 class ListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-//                    color: Colors.red,
-          ),
-      child: ListView.builder(
-        itemCount: counterTimers.length,
-        itemBuilder: (BuildContext context, int index) {
-          TouchCounter tCounter = counterTimers[index];
-          return Container(
-            child: Row(
-              children: <Widget>[
-                Text(tCounter.counter.toString()),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  formatDate(tCounter.datetime, [HH, ':', nn, ':', ss]),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(tCounter.diff.toString()),
-              ],
-            ),
-          );
-        },
+    return Padding(
+      padding: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(),
+        child: ListView.builder(
+          itemCount: counterTimers.length,
+          itemBuilder: (BuildContext context, int index) {
+            TouchCounter tCounter = counterTimers[index];
+            return Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    tCounter.counter.toString(),
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                  SizedBox(
+                    width: 10,
+                    height: 4,
+                  ),
+                  Text(
+                    formatDate(
+                      tCounter.datetime,
+                      [HH, ':', nn, ':', ss],
+                    ),
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                  SizedBox(
+                    height: 4,
+                    width: 10,
+                  ),
+                  Text(
+                    tCounter.diff.toString(),
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
