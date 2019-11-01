3f3d450587e01e75df0e6ec0821f28436370032b
dev.moonho@gmail.com <lgcns2020!!>
2019-11-01 22:38:37 +0900

    Debug 라벨 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,9 @@ class TouchCounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Touch Counter App',
       home: Home(),
     );
   }
 }
-
-
