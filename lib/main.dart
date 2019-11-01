8d208d091d234e28a5665250b595023c870e9e84
dev.moonho@gmail.com <lgcns2020!!>
2019-11-01 16:19:16 +0900

    기본기능 구현 - 화면 터치시 카운터 +1 - 화면구성 카운터 화면 / 리스트 화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:touch_counter_app/app_screens/home.dart';
+
+void main() => runApp(new TouchCounterApp());
+
+class TouchCounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Touch Counter App",
+      home: Scaffold(
+        body: Home(),
+      ),
+    );
+  }
+}
