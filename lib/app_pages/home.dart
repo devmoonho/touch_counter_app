6c3d424d4e343f970ab2ba2ec0004a800da09e2c
dev.moonho@gmail.com <lgcns2020!!>
2019-11-02 00:24:15 +0900

    ListView Builder 사용하여 Touch Counter Model 임시값 표시

--- lib/app_pages/home.dart
+++ lib/app_pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:touch_counter_app/models/touch_counter_model.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -20,7 +22,7 @@ class Home extends StatelessWidget {
                 child: Container(
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.blue,
+//                    color: Colors.blue,
                   ),
                   child: Text(
                     '1000',
@@ -35,15 +37,24 @@ class Home extends StatelessWidget {
                 child: Container(
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+//                    color: Colors.red,
                   ),
-                  child: ListView(
-                    children: <Widget>[
-                      Text('1000000000'),
-                      Text('1000000000'),
-                      Text('1000000000'),
-                      Text('1000000000'),
-                    ],
+                  child: ListView.builder(
+                    itemCount: counterTimers.length,
+                    itemBuilder: (BuildContext context, int index){
+                      TouchCounter tCounter = counterTimers[index];
+                      return Container(
+                        child: Row(
+                          children: <Widget>[
+                            Text(tCounter.counter.toString()),
+                            SizedBox(width: 10,),
+                            Text(formatDate(tCounter.datetime,[HH, ':', nn, ':', ss]),),
+                            SizedBox(width: 10,),
+                            Text(tCounter.diff.toString()),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
               )
