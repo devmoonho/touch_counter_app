91b506243e73b42f1ce01610eebdba40e0bf7f56
dev.moonho@gmail.com <lgcns2020!!>
2019-11-01 21:03:15 +0900

    TouchAppCounter App class 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(TouchCounterApp());
+
+class TouchCounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
+
