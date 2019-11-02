35f58cdcd9f3059de9b8588e903b33eb4c7b7048
dev.moonho@gmail.com <lgcns2020!!>
2019-11-03 02:40:39 +0900

    Provider에서 리스트뷰어 카운터 리스트를 받기위한 consumer 추가

--- lib/app_pages/widgets/list_widget.dart
+++ lib/app_pages/widgets/list_widget.dart
@@ -1,6 +1,8 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:touch_counter_app/models/touch_counter_model.dart';
+import 'package:touch_counter_app/providers/home_provider.dart';
 
 class ListWidget extends StatelessWidget {
   @override
@@ -12,75 +14,78 @@ class ListWidget extends StatelessWidget {
         decoration: BoxDecoration(
           color: Color(0xFFF6F6F6),
         ),
-        child: ListView.builder(
-          itemCount: counterTimers.length,
-          itemBuilder: (BuildContext context, int index) {
-            TouchCounter tCounter = counterTimers[index];
-            return Container(
-              margin: EdgeInsets.fromLTRB(2.0, 5.0, 2.0, 5.0),
-              height: 100.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    offset: Offset(1.0, 1.0),
-                    blurRadius: 6.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    child: Text(
-                      tCounter.counter.toString(),
-                      style: TextStyle(fontSize: 30.0),
+        child: Consumer<Counter>(
+          builder: (context, counter, child) => ListView.builder(
+            reverse: true,
+            itemCount: counter.touchCounters.length,
+            itemBuilder: (BuildContext context, int index) {
+              TouchCounter tCounter = counter.touchCounters[index];
+              return Container(
+                margin: EdgeInsets.fromLTRB(2.0, 5.0, 2.0, 5.0),
+                height: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 6.0,
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                    height: 4,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        formatDate(
-                          tCounter.datetime,
-                          [HH, ':', nn, ':', ss],
-                        ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        tCounter.counter.toString(),
                         style: TextStyle(fontSize: 30.0),
                       ),
-                      SizedBox(height: 5.0,),
-                      Text(
-                        formatDate(
-                          tCounter.datetime,
-                          [yyyy, '-', mm, '-', dd],
-                        ),
-                        style: TextStyle(fontSize: 20.0,color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 4,
-                    width: 10,
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Text(
-                      tCounter.diff.toString(),
-                      style: TextStyle(fontSize: 20.0),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
+                    SizedBox(
+                      width: 10,
+                      height: 4,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          formatDate(
+                            tCounter.datetime,
+                            [HH, ':', nn, ':', ss],
+                          ),
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                        SizedBox(height: 5.0,),
+                        Text(
+                          formatDate(
+                            tCounter.datetime,
+                            [yyyy, '-', mm, '-', dd],
+                          ),
+                          style: TextStyle(fontSize: 20.0,color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 4,
+                      width: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        tCounter.diff.inSeconds.toString(),
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
