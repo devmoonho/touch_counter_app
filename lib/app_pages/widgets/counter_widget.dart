bec99213ed93d22e389a4bb6b49c60bff95f624e
dev.moonho@gmail.com <lgcns2020!!>
2019-11-02 17:13:07 +0900

    Counter Widget에 InWell 효과 추가

--- lib/app_pages/widgets/counter_widget.dart
+++ lib/app_pages/widgets/counter_widget.dart
@@ -5,32 +5,40 @@ import 'package:touch_counter_app/providers/home_provider.dart';
 class CounterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Provider.of<Counter>(context, listen: false).increment(),
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
-        child: Padding(
-          padding: EdgeInsets.only(bottom: 5.0),
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 5.0),
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0.0, 5.0),
+                blurRadius: 10.0,
+              ),
+            ],
+          ),
+          child: SizedBox.expand(
+            child: Material(
               color: Theme.of(context).primaryColor,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0.0, 5.0),
-                  blurRadius: 10.0,
-                ),
-              ],
-            ),
-            child: Consumer<Counter>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Rubik',
-                  fontSize: 150,
+              child: InkWell(
+                onTap: () => Provider.of<Counter>(context, listen: false).increment(),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Consumer<Counter>(
+                    builder: (context, counter, child) => Text(
+                      '${counter.value}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Rubik',
+                        fontSize: 150,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
