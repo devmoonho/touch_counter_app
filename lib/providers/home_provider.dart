38300789f25096b043d903d22da1be6c29bb7dd8
dev.moonho@gmail.com <lgcns2020!!>
2019-11-03 02:38:57 +0900

    touchCounter 리스트 변수 추가 setCounter 함수추가 - touchCounter add item getDiff 함수추가 - 이전 인덱스 datetime과 현재 DateTime.now() 차이 반환

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:touch_counter_app/models/touch_counter_model.dart';
 
 class Counter with ChangeNotifier {
   int value = 0;
+  List<TouchCounter> touchCounters = [];
 
   void increment() {
+    setCounter(value);
     value += 1;
     notifyListeners();
   }
@@ -12,8 +15,21 @@ class Counter with ChangeNotifier {
     value > 0 ? value -= 1 : value = 0;
     notifyListeners();
   }
+
   void reset() {
     value = 0;
     notifyListeners();
+  }
+
+  void setCounter(int value) {
+    touchCounters.add(TouchCounter(counter: value, datetime: DateTime.now(), diff: getDiff(value)));
+  }
+
+  Duration getDiff(value) {
+    if (touchCounters.length == 0) {
+      return Duration(seconds:0);
+    } else {
+      return DateTime.now().difference(touchCounters[value - 1].datetime);
+    }
   }
 }
