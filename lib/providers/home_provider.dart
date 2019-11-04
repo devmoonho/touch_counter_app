ef56388bf427f08b3382e3de4be180ff1c57cd9e
moonho yeo <dev.moonho@gmail.com>
2019-11-05 03:31:08 +0900

    counterwidget 이 전체 커버되도록 CounterFill class 추가 gesture drage up/down notifyListeners 등록

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:touch_counter_app/models/touch_counter_model.dart';
 
+class CounterFill {
+  int flex;
+  bool offStage;
+  EdgeInsetsGeometry padding;
+  AlignmentGeometry alignment;
+
+  CounterFill() {
+    this.flex = 100;
+    this.offStage = false;
+    this.padding = EdgeInsets.only(top: 10.0);
+    this.alignment = Alignment.center;
+  }
+}
+
 class Counter with ChangeNotifier {
   int value = 0;
   List<TouchCounter> touchCounters = [];
   ScrollController scrollController = new ScrollController();
+
+  CounterFill counterFill = new CounterFill();
+  bool isCounterFill = false;
+
+  void gestureMove(double start, double end) {
+    print('start : $start / end : $end / upDown : ${end - start}');
+  }
+
+  void gestureUp() {
+    isCounterFill = false;
+    counterFill.flex = 100;
+    counterFill.offStage = false;
+    counterFill.padding = EdgeInsets.only(top: 10.0);
+    counterFill.alignment = Alignment.center;
+
+    print('isCounterFill $isCounterFill');
+    notifyListeners();
+  }
+
+  void gestureDown() {
+    isCounterFill = true;
+    counterFill.flex = 0;
+    counterFill.offStage = true;
+    counterFill.padding = EdgeInsets.only(bottom: 100.0);
+    counterFill.alignment = Alignment.bottomCenter;
+
+    print('isCounterFill $isCounterFill');
+    notifyListeners();
+  }
 
   void increment() {
     setCounter(value);
